98fc12cc21fd9d91dc009edee93133436c4598b6
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-06-05 17:50:07 +0300

    Short-Circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -64,13 +64,18 @@ console.log(true || 0);
 console.log(undefined || null);
 console.log(undefined || 0 || '' || 'Hello' || 23 || null);
 
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
-console.log(restaurant);
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+// console.log(restaurant);
 
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// restaurant.numGuests = '';
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests ?? 10;
+console.log(guests);
 
 console.log(`--------- AND ---------`);
 
