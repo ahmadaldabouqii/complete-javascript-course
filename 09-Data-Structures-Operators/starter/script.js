70196c78d98d7fd06355952cdbf02e79e659e1de
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-06-07 16:26:10 +0300

    Optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -98,6 +98,27 @@ const game = {
     team2: 6.5,
   },
 };
+
+/////////////////////////  Optional Chaining  /////////////////////////
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open); //error
+
+// WITH optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (const day of days) {
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day} we open at ${open}`);
+}
+
+//Methods
+console.log(restaurant.order?.(0, 1) ?? 'method does not exist!');
+console.log(restaurant.orderRissotto?.(0, 1) ?? 'method does not exist!');
 /////////////////////////  Looping Arrays: The for-of Loop  /////////////////////////
 
 const dobleMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
