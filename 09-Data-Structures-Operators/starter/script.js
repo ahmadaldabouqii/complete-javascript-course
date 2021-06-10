f42e70a12d776d09934afa0f13feeda9d639a639
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-06-10 16:25:10 +0300

    Finish Code challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -99,6 +99,46 @@ const game = {
   },
 };
 
+/////////////////////////  For Coding Challenge #3  /////////////////////////
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+// 1)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+// 2)
+gameEvents.delete(64);
+// console.log(gameEvents);
+
+// 3)
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4)
+for (const [min, event] of gameEvents) {
+  min <= 45
+    ? console.log(`[FIRST HALF] ${min}: ${event}`)
+    : console.log(`[SECOND HALF] ${min}: ${event}`);
+}
+
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
+/*
 /////////////////////////  Maps: Iteration  /////////////////////////
 const question = new Map([
   ['question', 'What is the best programming language in the world?'],
@@ -131,7 +171,7 @@ console.log([...question]);
 console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
-/*
+
 /////////////////////////  Maps  /////////////////////////
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
