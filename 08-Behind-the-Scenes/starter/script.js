45854ad4d2520bc830787a63d08c45aa4b75852a
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-06-23 12:36:04 +0300

    Primitives vs. Objects (Primitive vs. Reference Types) in practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -128,7 +128,7 @@ matilda.calcAge();
 
 const f = jonas.calcAge;
 f();
-*/
+
 
 //////////////////////////////    Regular Functions vs. Arrow Functions    //////////////////////////////
 
@@ -178,3 +178,61 @@ var addArrow = (a, b) => {
   return a + b;
 };
 addArrow(2, 5);
+
+
+//////////////////////////////    Primitives vs. Objects (Primitive vs. Reference Types)    //////////////////////////////
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Ahmad',
+  age: 24,
+};
+
+const friend = me;
+friend.age = 27;
+console.log('Friend: ', friend);
+console.log('Me: ', me);
+
+*/
+
+//////////////////////////////    Primitives vs. Objects (Primitive vs. Reference Types) in practice    //////////////////////////////
+
+// Primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage: ', marriedJessica);
+// marriedJessica = {};
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage: ', jessicaCopy);
