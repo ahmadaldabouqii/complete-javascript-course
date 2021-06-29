6ad31d6efeb0c888ee3b2b808320ebb3501644d4
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-06-29 16:15:17 +0300

    Looping Arrays: forEach

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,8 +71,8 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+/*
 /////////////////////////////////////////////////   141. Simple Array Methods   /////////////////////////////////////////////////
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
@@ -108,3 +108,32 @@ console.log([...arr, ...arr2]);
 
 // JOIN
 console.log(letters.join('-'));
+*/
+
+/////////////////////////////////////////////////   142. Looping Arrays: forEach   /////////////////////////////////////////////////
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('<----------FOREACH---------->');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
+
+/////////////////////////////////////////////////   143. forEach With Maps and Sets   /////////////////////////////////////////////////
