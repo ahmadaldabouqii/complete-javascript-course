5b5fd7d5f364bda82e67c3b3be31b849b5605a29
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-07-01 00:18:34 +0300

    150. The filter Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -88,13 +88,12 @@ const createUserNames = function (accs) {
   });
 };
 createUserNames(accounts);
-console.log(accounts);
 
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /*
 /////////////////////////////////////////////////   141. Simple Array Methods   /////////////////////////////////////////////////
 
@@ -227,3 +226,17 @@ const movementsDescriptions = movements.map(
 );
 console.log(movementsDescriptions);
 */
+
+/////////////////////////////////////////////////  150. The filter Method   /////////////////////////////////////////////////
+
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
